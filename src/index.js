b43f2b87879abe43cc28e78313d58d988652a173
hsootree <hsootree@gmail.com>
2017-09-19 12:11:33 +0900

    modified login

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const csurf = require('csurf')
 const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-
+const bcrypt = require('bcrypt')
 const util = require('./util')
 const query = require('./query')
 const mw = require('./middleware')
@@ -29,19 +29,52 @@ app.use(cookieSession({
     process.env.SECRET
   ]
 }))
+app.use(urlencodeMiddleware)
+app.use(csrfMiddleware)
 app.use(flash())
-app.use(csurf())
-app.use(mw.insertReq)
-app.use(mw.insertToken)
+app.set('view engine', 'pug')
 
 // passport 관련 미들웨어 삽입
 app.use(passport.initialize())
 app.use(passport.session())
 
-// passport가 유저 정보를 세션에 저장할 수 있도록 직렬화
 passport.serializeUser((user, done) => {
-  done(null, user.id)
+    // user 객체로부터 세션에 저장할 수 있는 문자열을 만들어서 반환
+    done(null, user.id)
+    done
+  })
+  // passport가 유저 정보를 세션에 저장할 수 있도록 직렬화
+passport.deserializeUser((id, done) => {
+  query.getUserById(id)
+    .then(user => {
+      if (user) {
+        done(null, jser)
+      } else {
+        done(new Error('아이디가 일치하는 사용자가 없습니다.'))
+      }
+    })
 })
+
+passport.use(new LocalStrategy((username, password, done) => {
+  // 인증정보와 일치하는 사용자가 있는지 확인
+  query.getUserById(username)
+    .then(matched => {
+      if (matched && bcrypt.compareSync(password, matched.password)) {
+        done(null, matched)
+      } else {
+        done(new Error('사용자가 이름 혹은 비밀번호가 일치하지 않습니다.')) // 보안상의 문제로 해킹을 할 때 경우의 수를 줄이는 것을 방지하기 위해서 둘중 하나가 틀린 것으로 알려줌. 보안과 접근성의 딜레마..
+      }
+    })
+}))
+
+function authMiddleware(req, res, next) {
+  if (req.user) {
+    // 로그인이 된 상태이므로 그냥 통과시킨다.
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
 
 // passport가 세션으로부터 유저 객체를 가져올 수 있도록 역직렬화
 passport.deserializeUser((id, done) => {
@@ -78,14 +111,14 @@ app.get('/', mw.loginRequired, (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-  res.render('login.pug')
+  res.render('login.pug', { errors: req.flash('error'), csrfToken: req.csrfToken() })
 })
 
 // passport-local을 통해 생성한 라우트 핸들러
-app.post('/login', passport.authenticate(('local'), {
-  successRedirect: '/', // 인증 성공 시 리다이렉트시킬 경로
-  failureRedirect: '/login', // 인증 실패 시 리다이렉트시킬 경로
-  failureFlash: '아이디 혹은 패스워드가 잘못되었습니다.' // 인증 실패 시 표시할 메시지
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
 }))
 
 app.get('/register', (req, res) => {
